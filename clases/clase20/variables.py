cbe4adc0ff2e13f52c35958b46264b7e9ab194dd
Gabriel Luque <GabJL@users.noreply.github.com>
2023-12-03 11:38:09 +0100

    Create variables.py

---
+++ clases/clase20/variables.py
@@ -0,0 +1,17 @@
+# Ejercicio 1 y 2
+
+Málaga: dict = {"enero": 12, "febrero": 12, "marzo": 14, "abril": 16, "mayo": 19, "junio": 23, "julio": 26, "agosto": 26,"septiembre": 23, "octubre": 19, "noviembre": 15, "diciembre": 13}
+
+Yakustk: dict = {"enero": -38, "febrero": -34, "marzo": -20, "abril": -5, "mayo": 8, "junio": 16, "julio": 20, "agosto": 15, "septiembre": 6, "octubre": -8, "noviembre": -27, "diciembre": -37}
+
+# Ejercicio 3
+Salud: dict = {
+  "Bioquímica estructural": [0.66, 0.61, 0.47, 0.64, 0.92, 0.92, 0.75, 0.56],
+  "Cálculo": [0.55, 0.52, 0.48, 0.29, 0.29, 0.06, 0.17, 0.53],
+  "Fundamentos de la programación": [0.58, 0.66, 0.53, 0.76, 0.7, 0.6, 0.49, 0.68],
+  "Física I": [0.23, 0.41, 0.38, 0.45, 0.37, 0.4, 0.56, 0.35],
+  "Álgebra lineal": [0.48, 0.44, 0.46, 0.54, 0.61, 0.41, 0.51, 0.61]
+}
+
+# Ejercicio 4
+peli = "Puss in Boots#Chris Miller#Antonio Banderas, Salma Hayek, Zach Galifianakis, Billy Bob Thornton#Animation, Adventure, Comedy#2011 6.6 90 149260000.0#An outlaw cat, his childhood egg-friend, and a seductive thief kitty set out in search for the eggs of the fabled Golden Goose to clear his name, restore his lost honor, and regain the trust of his mother and town."
